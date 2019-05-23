e427230a8724e3666179b5803fd03b55fe0d9b90
alan <info@iscbj.com>
2019-05-23 23:38:19 +0800

    Application Models

---
+++ Chapter05/r1_model_attr/my_library/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'My Library',
+    'summary': '轻松管理图书',
+    'description': """长描述占位""",
+    'author': 'Alan Hou',
+    'website': 'https://alanhou.org',
+    'category': 'Uncategorized',
+    'version': '12.0.1',
+    'depends': ['base'],
+    'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'views/library_book.xml',
+    ],
+}
---
+++ Chapter05/r1_model_attr/my_library/models/library_book.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+
+class LibraryBook(models.Model):
+    _name = 'library.book'
+    _description = 'Library Book'
+    _order = 'date_release desc, name'
+    _rec_name = 'short_name'
+
+    name = fields.Char('Title', required=True)
+    short_name = fields.Char('Short Title', required=True)
+    date_release = fields.Date('Release Date')
+    author_ids = fields.Many2many(
+        'res.partner',
+        string='Authors'
+    )
+
+    def name_get(self):
+        result = []
+        for record in self:
+            rec_name = "%s (%s)" % (record.name, record.date_release)
+            result.append((record.id, rec_name))
+        return result
